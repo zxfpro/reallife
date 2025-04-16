14e5cea530e3cb498ec7ce089568cbabe46cdb22
823042332@qq.com <823042332@qq.com>
2025-04-16 23:10:45 +0800

    clean

--- create_package.sh
+++
@@ -1,7 +0,0 @@
-uv init .      
-uv sync
-mkdocs new .
-cp ../tools/run_build.sh .
-cp ../tools/run_test.sh .
-cp ../tools/update_docs.sh .
-bash update_docs.sh 
---
+++ test /test_.py
@@ -0,0 +1,6 @@
+import sys
+import os
+# 将项目根目录添加到 Python 路径
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import re
+import pytest
